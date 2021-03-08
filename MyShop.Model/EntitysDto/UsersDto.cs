80df967ee3cc6812e9f65f7b88711020da7cd445
Tiny-Zhang <zh_jin123@163.com>
2021-03-08 20:17:12 +0800

    添加基础类

---
+++ MyShop.Model/EntitysDto/UsersDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyShop.Model.EntitysDto
+{
+    public class UsersDto
+    {
+        public string Username { get; set; }
+        public string TrueName { get; set; }
+        public string Email { get; set; }
+        public string Head { get; set; }
+        public string Mobile { get; set; }
+        /// <summary>
+        /// 0未锁定  1锁定
+        /// </summary>
+        public string Status { get; set; }
+        public string CreatTime { get; set; }
+    }
+}
