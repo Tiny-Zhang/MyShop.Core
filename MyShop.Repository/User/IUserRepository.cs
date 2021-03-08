d20c400f129b8e90a7ec95077781200f6f56fc1a
Tiny-Zhang <zh_jin123@163.com>
2021-03-08 20:18:07 +0800

    数据访问层

---
+++ MyShop.Repository/User/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Model.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Repository
+{
+    public interface IUserRepository
+    {
+        Users QueryUserInfo(string name);
+        Task<Users> QueryUserInfoAsync(string name);
+    }
+}
---
+++ MyShop.Repository/User/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Options;
+using MyShop.Common;
+using MyShop.Model.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDatabase database;
+        private readonly IOptionsSnapshot<DBConfig> dbConfig;
+
+        public UserRepository(IDatabase _database, IOptionsSnapshot<DBConfig> _dbConfig)
+        {
+            database = _database;
+            dbConfig = _dbConfig;
+        }
+
+        public Users QueryUserInfo(string name)
+        {
+            string sql = @"SELECT top 1 [Userid],[Username],[Password],[TrueName],[Sex],[Email],
+                           [Birthday],[Head],[Mobile],[status],[islock],[regtime]
+                           FROM [Users](nolock)
+                           where Username=@Username";
+            try
+            {
+                var result = database.Query<Users>(dbConfig.Value.DBReadOnlyConString, sql, new { Username = name}).FirstOrDefault();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                //日志
+                return null;
+            }
+        }
+
+        public Task<Users> QueryUserInfoAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
