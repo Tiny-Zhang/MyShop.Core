1c78246618c7d4b81d88af4bf0e165d993e52c59
Tiny-Zhang <zh_jin123@163.com>
2021-03-11 18:26:56 +0800

    2131

---
+++ MyShopApi/Extensions/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShopApi.Extensions
+{
+    /// <summary>
+    /// AutoMapper 全局注册类
+    /// </summary>
+    public class AutoMapperConfig
+    {
+        public static MapperConfiguration AutoMapperRegister()
+        {
+            return new MapperConfiguration(cfg =>
+             {
+                 cfg.AddProfile(new EntityToDtoProfile());
+             });
+        }
+    }
+}
