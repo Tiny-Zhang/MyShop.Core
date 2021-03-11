cc32e87d408b2292bea56b56b273751da59d59a7
Tiny-Zhang <zh_jin123@163.com>
2021-03-11 18:26:00 +0800

    地方

---
+++ MyShopApi/Extensions/DtoForEntityProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShopApi.Extensions
+{
+    public class DtoForEntityProfile: Profile
+    {
+        /// <summary>
+        /// Dto类-->实体类
+        /// </summary>
+        public DtoForEntityProfile()
+        {
+
+        }
+    }
+}
